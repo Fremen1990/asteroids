e6769be4eb1ea129e9aa256330528765efb1f52d
Fremen1990 <fremen1990@gmail.com>
2025-01-21 16:18:40 +0100

    Sprites

--- main.py
+++ main.py
@@ -9,11 +9,17 @@ def main():
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
 
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
         screen.fill("black")
         for event in pygame.event.get():
+            deltaTime = clock.tick(60)
+            dt = deltaTime / 1000
             if event.type == pygame.QUIT:
                 return
 
