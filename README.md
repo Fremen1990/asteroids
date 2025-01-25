aed5288aad01b221fe6f72c55ab66d7fad9ac453
Fremen1990 <fremen1990@gmail.com>
2025-01-25 19:45:17 +0100

    Acceleration

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
-### Asteroid Game in Python 🐍
+## Asteroid Game in Python 🐍
 
 Extending project with below ideas:
 
 - [x] Add a scoring system
 - [x] Implement multiple lives and respawning
 - [x] Add an explosion effect for the asteroids
-- [ ] Add acceleration to the player movement
-- [ ] Make the objects wrap around the screen instead of disappearing
-- [ ] Add a background image
-- [ ] Create different weapon types
+- [x] Add acceleration to the player movement
 - [ ] Make the asteroids lumpy instead of perfectly round
+- [ ] Add a background image
 - [ ] Make the ship have a triangular hit box instead of a circular one
+- [ ] Make the objects wrap around the screen instead of disappearing
+- [ ] Create different weapon types
 - [ ] Add a shield power-up
 - [ ] Add a speed power-up
 - [ ] Add bombs that can be dropped
--- player.py
+++ player.py
@@ -1,18 +1,12 @@
 import pygame
-from pygame.transform import rotate
 
-from asteroids.constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from constants import PLAYER_COLOR
-from constants import PLAYER_WIDTH
-# from constants import PLAYER_LIVES
-from constants import PLAYER_SHOOT_SPEED
-from constants import PLAYER_SPEED
-from constants import PLAYER_TURN_SPEED
-from constants import PLAYER_SHOOT_COOLDOWN
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_COLOR, PLAYER_WIDTH, PLAYER_SPEED, PLAYER_TURN_SPEED, \
+    PLAYER_ACCELERATION, PLAYER_FRICTION, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN
 from circleshape import CircleShape
 from shot import Shot
 
 from constants import PLAYER_RADIUS
+
 
 class Player(CircleShape):
     def __init__(self, x, y, shots):
@@ -24,6 +18,10 @@ class Player(CircleShape):
         self.is_invulnerable = False
         self.invulnerable_timer = 0
 
+        self.velocity = pygame.Vector2(0, 0)
+        self.acceleration = PLAYER_ACCELERATION
+        self.friction = PLAYER_FRICTION
+
     def triangle(self):
         forward = pygame.Vector2(0, -1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -34,10 +32,10 @@ class Player(CircleShape):
 
     def draw(self, screen):
         if self.is_invulnerable:
-            if pygame.time.get_ticks() % 500 <  250:
-                pygame.draw.polygon(screen,PLAYER_COLOR, self.triangle(), PLAYER_WIDTH)
+            if pygame.time.get_ticks() % 500 < 250:
+                pygame.draw.polygon(screen, PLAYER_COLOR, self.triangle(), PLAYER_WIDTH)
         else:
-            pygame.draw.polygon(screen,PLAYER_COLOR, self.triangle(), PLAYER_WIDTH)
+            pygame.draw.polygon(screen, PLAYER_COLOR, self.triangle(), PLAYER_WIDTH)
 
     def rotate(self, dt):
         self.rotation = self.rotation + PLAYER_TURN_SPEED * dt
@@ -51,39 +49,58 @@ class Player(CircleShape):
         keys = pygame.key.get_pressed()
         self.shoot_timer -= dt
 
+        # Handle rotation
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
-        if keys[pygame.K_w]:
-            self.move(dt)
-        if keys[pygame.K_s]:
-            self.move(-dt)
 
+        # Handle movement
+        accelerating = keys[pygame.K_w]
+        self.move(dt, accelerating)
+
+        # Screen wrapping logic
+        if self.position.x < 0:
+            self.position.x = SCREEN_WIDTH
+        elif self.position.x > SCREEN_WIDTH:
+            self.position.x = 0
+        if self.position.y < 0:
+            self.position.y = SCREEN_HEIGHT
+        elif self.position.y > SCREEN_HEIGHT:
+            self.position.y = 0
+
+        # Shooting logic
         if keys[pygame.K_SPACE]:
-            if self.shoot_timer >0:
+            if self.shoot_timer > 0:
                 return
             self.shoot_timer = PLAYER_SHOOT_COOLDOWN
             self.shoot()
+
 
     def lose_life(self):
         # self.lives -= -1
         self.respawn()
 
     def respawn(self):
-        self.position = pygame.Vector2(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+        self.position = pygame.Vector2(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
         self.rotation = 0
         self.is_invulnerable = True
         self.invulnerable_timer = 3
+        self.velocity = pygame.Vector2(0, 0)
 
-    def move(self, dt):
-        forward = pygame.Vector2(0,-1).rotate(self.rotation)
-        self.position += forward * PLAYER_SPEED * dt
+    def move(self, dt, accelerating):
+        forward = pygame.Vector2(0, -1).rotate(self.rotation)
+
+        if accelerating:
+            self.velocity += forward * self.acceleration * dt
+
+        self.velocity *= self.friction
+        self.position += self.velocity * dt
 
     def shoot(self):
         shot = Shot(self.position.x, self.position.y)
-        forward = pygame.Vector2(0,-1)
+        forward = pygame.Vector2(0, -1)
         velocity = forward.rotate(self.rotation) * PLAYER_SHOOT_SPEED
-        shot.velocity  = velocity
+        shot.velocity = velocity
 
         self.shots.add(shot)
